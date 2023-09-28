90ca77135c635a29b1613615bb7634efbf606589
shweta <junnarkarshweta3@gmail.com>
2023-09-28 18:21:56 +0530

    Interface variable are of public, static, final types.

--- InterfaceVariableType.java
+++ InterfaceVariableType.java
@@ -1,17 +1,13 @@
-
-
 public class InterfaceVariableType {
     public static void main(String[] args) {
         System.out.println(Testing.a); //interface variables are public, static , final.
        // Testing.a=10; ......it gives an error final value can not change.
     }
 }
-interface varpractices
+interface Varpractices
 {
     int a = 0;
 }
-class Testing implements varpractices
+class Testing implements Varpractices
 {
-
-
 }
